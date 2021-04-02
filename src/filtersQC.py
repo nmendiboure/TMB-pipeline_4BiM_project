d4400c2568673d1bab743d5764915a021a5159cd
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2021-04-02 15:18:32 +0200

    Correction fonction quality_filter_tumor

--- src/filtersQC.py
+++ src/filtersQC.py
@@ -74,11 +74,12 @@ def quality_filter_tumor(df_tumor, index_normal, reject = ['LowQual', 'INDEL_SPE
     for chrom_pos in index_normal: #chrom_pos : tuple (#CHROM, #POS)
         chrom = chrom_pos[0] #CHROM
         pos = chrom_pos[1] #POS
-        if(pos in df_tumor["POS"].values): #on regarde si la position indexee de df_normal est aussi dans df_tumor
+        if(pos in df_tumor["POS"].loc[df_tumor["CHROM"] == chrom].values): #on regarde si la position indexee de df_normal est aussi dans df_tumor
             tmp_index = int(np.argwhere(df_tumor["POS"].loc[df_tumor["CHROM"] == chrom].values == pos)) #indexe de cette position
+            #print(chrom, pos, tmp_index)
             df_t = df_t.drop(labels = tmp_index, axis=0) #on supprime la ligne ou il y a cette position
-
-    df_t.index = range(0, len(df_t), 1) #reajustement des indexes apres le drop
+              
+        df_t.index = range(0, len(df_t), 1) #reajustement des indexes apres le drop
     
     for muta in df_t.index: #idem que dans quality_filter_normal, on enleve les filters de mauvaise qualite
         if df_t["FILTER"][muta] in reject:
