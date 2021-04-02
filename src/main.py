f4cfb1a0b73524899bfaa9012e8e0b63abdd0373
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2021-04-02 22:11:30 +0200

    Amelioration des inputs pour le filtrage des chromosomes

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import filtersQC
 import TMB
 
 import copy
+import os
 import sys
 import pandas as pd
 import numpy as np
@@ -54,7 +55,7 @@ if __name__ == "__main__":
 	####################### Importation des données ######################################
 	######################################################################################
 	print("Bienvenue dans notre pipeline pour le calcul d'un TMB.")
-	print("Veuillez indiquer le chemin realtif vers les fichiers VCF (tumor et normal) :")
+	print("Veuillez indiquer le chemin realtif vers les fichiers VCF (tumor et normal) :", "\n")
 	
 	path_tumor = str( input ("VCF tumoral : ") )
 	path_normal = str( input ("VCF normal : ") )
@@ -62,7 +63,7 @@ if __name__ == "__main__":
 	
 	################ Controle qualité / verification fichier conforme ####################
 	######################################################################################
-	QC = input("Voulez vous un contrôle qualité sur vos fichier VCF ? [o/n]")
+	QC = input("Voulez vous un contrôle qualité sur vos fichier VCF ? [o/n]  ")
 	
 	
 	if (QC.lower() == "o") or (QC.lower() == "oui") or (QC.lower() == "y") or (QC.lower() == "yes"): #si oui 
@@ -76,10 +77,19 @@ if __name__ == "__main__":
 	################ Selection des chromosomes (si besoin) ###############################
 	######################################################################################
 		
-	keep_chrom = input("Voulez vous ne conserver seulement qu'un ou plusieurs chromosomes en particulier ? [o/n]")
+	keep_chrom = input("Voulez vous ne conserver seulement qu'un ou plusieurs chromosomes en particulier ? [o/n]  ")
+	if (keep_chrom.lower() == "o") or (keep_chrom.lower() == "oui") or (keep_chrom.lower() == "y") or (keep_chrom.lower() == "yes"): #si oui 
+		chr2keep = []
+		possible_answers = [str(i) for i in range(1, 23)] + ['X'] +['Y'] + ['MT'] 
+		chr2keep.append(str(input("Veuillez indiquer quel chromosome vous souhaitez conserver : ")))
+		
+		while True :
+			chr_ = str(input("Veuillez indiquer un autre chromosome que vous souhaitez conserver (tapez q pour sinon) : "))
+			if (chr_ not in possible_answers):
+				break
+			else:
+				chr2keep.append(chr_)
 	
-	if (keep_chrom.lower() == "o") or (keep_chrom.lower() == "oui") or (keep_chrom.lower() == "y") or (Qkeep_chrom.lower() == "yes"): #si oui 
-		chr2keep = list(input("Veuillez indiquer quel(s) chromosome(s) vous souhaitez conserver (sans virgule, ex : 1 2 X ...) :").split(" "))
 		df_tumor = select_chr(df_tumor, chr2keep)
 		df_normal = select_chr(df_normal, chr2keep)
 		print("Les chromosomes suivants ont bien été conservés dans vos dataframes: ", *chr2keep, sep = "\n")
@@ -88,15 +98,39 @@ if __name__ == "__main__":
 	
 	################ Controle qualité des FILTER #########################################
 	######################################################################################
-	QC_filters = input("Voulez vous filtrer les variants de mauvaise qualité ? [o/n]")
+	QC_filters = input("Voulez vous filtrer les variants de mauvaise qualité ? [o/n]  ")
 	
 	if (QC_filters.lower() == "o") or (QC_filters.lower() == "oui") or (QC_filters.lower() == "y") or (QC_filters.lower() == "yes"): #si oui 
 		# F is for FILTERED
 		F_df_tumor, F_df_normal = filtersQC.global_filter(df_tumor, df_normal)
 	######################################################################################
 	
+	"""
+	################ ANNOVAR #############################################################
+	######################################################################################
 	
+	#somatic_path = "./annovar/sample1/Sample1_pre_somatic_chr1.vcf"
+	#avintput_path = "./annovar/sample1/Sample1_pre_somatic_chr1.avinput"
+	#somatic_exonic_path = "./annovar/sample1/sample1_post_somatic_chr1"
 	
+	print("Nous allons maintenant procéder à l'analyse des variant à l'aide du logiciel ANNOVAR.")
+	print("Avant de commencer, assurez vous de bien avoir le logiciel ANNOVAR (dossier /annovar/) dans le répertoire principale de ce pipeline")
 	
+	somatic_path = input("Veuillez renseigner le chemin relatif pour votre fichier VCF somatique : ")
+	avinput_path = input("Veuillez renseigner la localisation suivie du nom du fichier avinput : ")
+	somatic_path = input("Veuillez renseigner la localisation suivie du nom du fichier exonic_variant_function : ")
 	
+	#1) convertir notre fichier vcf au format .avinput utilisé par annovar
+	cmd0 = "perl " +  "./annovar/convert2annovar.pl -format vcf4 " + str(somatic_path) + " > " + str(avinput_path)
+	os.system(cmd0)
+	
+	#2) télécharger la bdd
+	cmd1 = "perl " + "./annovar/annotate_variation.pl -downdb -buildver hg19 -webfrom annovar refGene " + "./annovar/humandb/"
+	os.system(cmd1)
+	
+	#3) Annoter le fichier avinput
+	cmd2 = "perl "  + "./annovar/annotate_variation.pl -out " + str(somatic_exonic_path) + " -build hg19 " + str(avinput_path) + " ./annovar/humandb/" 
+	os.system(cmd2)
+	
+	print("Annotation des variants avec ANNOVAR faite.")"""
 		
