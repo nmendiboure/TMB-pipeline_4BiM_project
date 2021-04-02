132fe006da5d52b60ffa5c19431b735fe6c4e9ea
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2021-04-02 13:15:09 +0200

    Ajout de la fonction select_chr

--- src/main.py
+++ src/main.py
@@ -12,6 +12,42 @@ from collections import Counter
 #VCF tumoral : ../vcf_files/sample1/Sample1_tumor_dna_chr1.vcf
 #VCF normal : ../vcf_files/sample1/Sample1-PBMC_normal_dna_chr1.vcf
 
+def select_chr(df, chrom):
+    """
+    Arguments :
+    
+        df : dataframe.
+        
+        chrom : int/string/list, le ou les chromosomes à conserver.
+        
+        Cette fonction permet de ne conserver qu'un ou plusieurs chromosomes parmi l'ensemble du dataframe,
+    afin de réduire le dataframe et ainsi reduire les temps de chargement pour la suite de ce pipeline
+    (ex : calcul du TMB qui peut être très long selon la taille du fichier etc ..)
+    
+    Return :
+    
+        new_df : dataframe n'ayant conservé que l'information sur les chromosomes renseignés par l'utilisateur.
+    """
+    new_df = copy.deepcopy(df)
+    
+    if (type(chrom) == int) or (type(chrom) == str): #Garder un seul chromosome
+        new_df = df.loc[df["CHROM"] == str(chrom)]
+        new_df.index = range(0, len(new_df), 1)  #reajustement des indexes
+        return(new_df)
+    
+    elif (type(chrom) == list): #Conserver plusieurs chromosomes
+        chrom = sorted(chrom) # numero chromosome dans l'ordre croissant
+        new_df = df.loc[df["CHROM"] == str(chrom[0])]
+        for i in range(1, len(chrom), 1):
+            tmp_df = df.loc[df["CHROM"] == str(chrom[i])]
+            new_df = pd.concat([new_df, tmp_df])
+            #print(len(tmp_df), len(new_df))
+            
+        new_df.index = range(0, len(new_df), 1)
+        print("Seuls les chromosomes suivants ont bien été conservés : ", *chrom, sep = "\n")
+        return(new_df)
+        
+
 if __name__ == "__main__":
 	print("Bienvenue dans notre pipeline pour le calcul d'un TMB.")
 	print("Veuillez indiquer le chemin realtif vers les fichiers VCF (tumor et normal) :")
