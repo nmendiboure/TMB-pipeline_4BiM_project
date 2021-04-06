87bad12cc0b183a0b0bfb21fb989bebd2b45621e
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2021-04-06 15:06:26 +0200

    Corrections mineures + optimisation code

--- src/case1.py
+++ src/case1.py
@@ -7,13 +7,16 @@ import TMB
 if __name__ == "__main__":
 
 	samples_path = "./samples/"
-	################ Importation des données #############################################
+
+	_YES_ = ['o', 'y', 'oui', 'yes', 'ok']
+	_NO_ = ['n', 'no', 'non']
+
+	####################### Importation des données ######################################
 	######################################################################################
+	print("Veuillez indiquer le nom des fichiers VCF tumoral et normal (sans l'extension .vcf ) dans le répertoire samples :", "\n")
 
-	print("Veuillez indiquer le chemin relatif vers le fichier VCF somatique :", "\n")
-	path_somatic = samples_path + str( input ("VCF somatique : ") )
-
-	print(path_somatic)
+	path_somatic = samples_path + str(input("VCF somatic : ")) + ".vcf"
+	print("Chemin vers le vcf somatic : ", path_somatic, "\n")
 
 	######################################################################################
 
@@ -23,7 +26,7 @@ if __name__ == "__main__":
 	print("Nous allons à présent effectuer le calcul du TMB : ", "\n")
 	size_WES = int(input("Veuillez spécifier la taille de l'exome de référence : "))
 
-	TMB = TMB.Compute_TMB_with_somatic( somatic_infile= str(path_somatic), exome_length = size_WES)
+	TMB = TMB.TMB_somatic( somatic_infile= str(path_somatic), exome_length = size_WES)
 
 	print("Votre taux de mutations TMB vaut : {}".format(TMB))
 
